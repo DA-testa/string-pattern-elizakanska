31329db03dc2425422ac49eb93a7e35456ba155b
Elīza Kanska 221RDB095 <78021756+elizakanska@users.noreply.github.com>
2023-04-15 12:19:22 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,53 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
+    # this function acquires input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     
+    inType = input().rstrip()
     
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
+    if inType == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif inType == 'F':
+        # might need adjustments, will see when testing
+        filename = input().rstrip()
+        with open(filename) as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
     
     # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (pattern, text)
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
+    # this function controls output, it doesn't need or have any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
-
-    # and return an iterable variable
-    return [0]
-
+    # this function finds the occurrences using Rabin Karp algorithm
+    
+    occurrences = []
+    
+    if len(pattern) > len(text):
+        return occurrences
+    
+    hash_pttrn = hash(pattern)
+    hash_txt = hash(text[:len(pattern)])
+    
+    if hash_pttrn == hash_txt and pattern == text[:len(pattern)]:
+        occurrences.append(0)
+    
+    prime = 2**31 - 1
+    h = pow(2, 8*(len(pattern)-1), prime) # 256^(m-1)
+    
+    for i in range(1, len(text)-len(pattern)+1):
+        hash_txt = ((hash_txt - ord(text[i-1])*h)*256 + ord(text[i+len(pattern)-1])) % prime
+        
+        if hash_txt == hash_pttrn and pattern == text[i:i+len(pattern)]:
+            occurrences.append(i)
+    
+    return occurrences
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
